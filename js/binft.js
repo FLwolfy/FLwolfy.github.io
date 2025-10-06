d2249715bc45a78967df9ad330bdfbb59892abf9
Hsuan-Kai Liao <hsuankailiao@gmail.com>
2025-10-06 12:31:18 +0000

    deploy: FLwolfy/BlogX@1fdac111a53d3e01038d1d794bb4df5aa8945d7f

---
+++ js/binft.js
@@ -0,0 +1,63 @@
+function binft(targetElement, content) {
+  function randomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+  function randomChar() {
+    return String.fromCharCode(94 * Math.random() + 33);
+  }
+  function noise(n) {
+    const frag = document.createDocumentFragment();
+    for (let i = 0; i < n; i++) {
+      const span = document.createElement("span");
+      span.textContent = randomChar();
+      span.style.color = randomColor();
+      frag.appendChild(span);
+    }
+    return frag;
+  }
+  function play() {
+    if (state.step > 0) {
+      state.step--;
+    } else {
+      state.step = stepGap;
+      if (state.prefixP < prefix.length) {
+        if (state.prefixP >= 0) state.text += prefix[state.prefixP];
+        state.prefixP++;
+      } else if (state.skillP < content.length) {
+        state.text += content[state.skillP++];
+      } else {
+        targetElement.textContent = state.text;
+        return; // ✅ 播放完即停
+      }
+    }
+    targetElement.textContent = state.text;
+    targetElement.appendChild(noise(
+      state.prefixP < prefix.length
+        ? Math.min(noiseChars, noiseChars + state.prefixP)
+        : Math.min(noiseChars, content.length - state.skillP)
+    ));
+    setTimeout(play, delay);
+  }
+
+  const prefix = "";
+  const stepGap = 1;
+  const delay = 75;
+  const noiseChars = 5;
+  const colors = [
+    "rgb(255,255,255)",
+    "rgb(220,220,255)",
+    "rgb(200,230,255)",
+    "rgb(180,255,255)",
+    "rgb(160,255,230)"
+  ];
+
+
+  const state = {
+    text: "",
+    prefixP: -noiseChars,
+    skillP: 0,
+    step: stepGap
+  };
+
+  play();
+}
